6442570fd4755a21568b3a1c6f39b8a56dc297b1
YusefQuinlan <39866937+YusefQuinlan@users.noreply.github.com>
2019-11-04 12:58:18 -0500

    Create 1.1.1_Basics_print.swift

---
+++ Basics/1.1.1_Basics_print.swift
@@ -0,0 +1,23 @@
+import Foundation
+print("Hello world")
+//the print("Something") function allows users to print things to the console
+// Such as the patterns below
+print("p")
+print(" p")
+print("  p")
+print(" p")
+print("p")
+print("p")
+print("  p")
+print("     p")
+print("  p")
+print("p")
+//Numbers can also be printed
+print(45)
+print(900)
+//The following two print statements won't work because they are not typed
+// correctly, the computer cannot distinguish that Print with a capital p 
+// is the 'print' function, and with a missing ')' the computer sees the
+// print statement as incorrect.
+print(90
+Print(99)
